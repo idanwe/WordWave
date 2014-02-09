d3803b4b79f52245550a5ba356645a6bcb4b996f
Idan Wender <idanwe2@gmail.com>
2014-02-09 23:56:00 +0200

    add all missing letters

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,33 +12,32 @@ angular.module('WordWaveApp')
       "ז": window.zai,
       "ח": window.het,
       "ט": window.tet,
-      // "י": window.yud,
-      // "כ": window.kaf,
-      // "ל": window.lamed,
-      // "מ": window.mem,
-      // "נ": window.nun,
-      // "ס": window.sameh,
-      // "ע": window.aayn,
-      // "פ": window.pay,
-      // "צ": window.ztadic,
-      // "ק": window.kuf,
-      // "ר": window.raish,
-      // "ש": window.shin,
-      // "ת": window.taf
+      "י": window.yud,
+      "כ": window.kaf,
+      "ל": window.lamed,
+      "מ": window.mem,
+      "נ": window.nun,
+      "ס": window.sameh,
+      "ע": window.aayn,
+      "פ": window.pay,
+      "צ": window.ztadic,
+      "ק": window.kuf,
+      "ר": window.raish,
+      "ש": window.shin,
+      "ת": window.taf
     };
 
     $scope.currentLetter = "א";
     $scope.words = $scope.letters[$scope.currentLetter];
-    $scope.currentWordIndex = 0
+    $scope.currentWordIndex = 1
 
-    $scope.switchLetter = function(letter) {
-      $scope.currentLetter = letter;
+    $scope.switchLetter = function() {
       $scope.words = $scope.letters[$scope.currentLetter];
-      $scope.currentWordIndex = 0
+      $scope.currentWordIndex = 1
     };
 
     $scope.progressPrecentes = function() {
-      return $scope.currentWordIndex / ($scope.words.length - 1) * 100
+      return $scope.currentWordIndex / ($scope.words.length) * 100
     };
 
     $scope.nextWord = function() {
@@ -56,10 +55,10 @@ angular.module('WordWaveApp')
     };
 
     $scope.isNextDisabled = function() {
-      return $scope.currentWordIndex == $scope.words.length - 1
+      return $scope.currentWordIndex === $scope.words.length
     };
 
     $scope.isPreviousDisabled = function() {
-      return $scope.currentWordIndex == 0
+      return $scope.currentWordIndex === 1
     }
   });
